ac5e2112f27740e159e09975c30cca7d5a825990
Awelless <hahaha.funny.yellow.dog@gmail.com>
2021-02-24 17:08:00 +0300

    fixes

--- 02.1. Repair/src/main/main.h
+++ 02.1. Repair/src/main/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 double calculateWallpaperArea(double roomX, double roomY, int windowsNumber, int doorNumber);
 int calculateWallpaperTubeNumber(double wallpaperArea, double wallpaperTubeLength);
 double calculateTotalCost(int wallpaperTubeNumber, double wallpaperTubeCost);
+
+#endif
